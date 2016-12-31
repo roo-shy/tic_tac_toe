facb2a11e537d1b19d31c7598c4c8c12d79104b0
at-rooshee <ru_sha_nia@yahoo.ca>
2016-12-30 23:42:24 -0600

    Adding winner and reset functions

--- tictactoe.js
+++ tictactoe.js
@@ -2,10 +2,36 @@ var player = 1;
 function clickBtn(btn) {
   if (player == 1) {
   document.getElementById(btn).value = "X";
+  document.getElementById(btn).disabled = "disabled";
   player -= 1;
+  winner();
   } 
-    else {
-    document.getElementById(btn).value = "0";
-    player += 1;
-    }
+  else {
+  document.getElementById(btn).value = "0";
+  document.getElementById(btn).disabled = "disabled";
+  player += 1;
+  winner();
+  }
+}
+
+function winner() {
+  if(document.getElementById("btn1").value == "X"
+  && document.getElementById("btn2").value == "X"
+  && document.getElementById("btn3").value == "X"
+){
+  alert("The Winner is X");
+  reset();
+}
+}
+
+function reset() {
+  document.getElementById("btn1").value = "";
+  document.getElementById("btn2").value = "";
+  document.getElementById("btn3").value = "";
+  document.getElementById("btn4").value = "";
+  document.getElementById("btn5").value = "";
+  document.getElementById("btn6").value = "";
+  document.getElementById("btn7").value = "";
+  document.getElementById("btn8").value = "";
+  document.getElementById("btn9").value = "";
 }
