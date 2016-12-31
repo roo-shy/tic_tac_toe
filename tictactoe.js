e1fc6fea93d3c96d1f44c4f8978ae6211ebded18
at-rooshee <ru_sha_nia@yahoo.ca>
2016-12-30 23:49:53 -0600

    Editing js

--- tictactoe.js
+++ tictactoe.js
@@ -15,9 +15,24 @@ function clickBtn(btn) {
 }
 
 function winner() {
-  if(document.getElementById("btn1").value == "X"
-  && document.getElementById("btn2").value == "X"
-  && document.getElementById("btn3").value == "X"
+  if(document.getElementById("btn1").value == "X" && 
+  document.getElementById("btn2").value == "X" &&
+  document.getElementById("btn3").value == "X" ||
+  document.getElementById("btn4").value == "X" && 
+  document.getElementById("btn5").value == "X" &&
+  document.getElementById("btn6").value == "X" ||
+  document.getElementById("btn7").value == "X" && 
+  document.getElementById("btn8").value == "X" &&
+  document.getElementById("btn9").value == "X" ||
+  document.getElementById("btn1").value == "X" && 
+  document.getElementById("btn4").value == "X" &&
+  document.getElementById("btn7").value == "X" ||
+  document.getElementById("btn2").value == "X" && 
+  document.getElementById("btn5").value == "X" &&
+  document.getElementById("btn8").value == "X" ||
+  document.getElementById("btn3").value == "X" && 
+  document.getElementById("btn6").value == "X" &&
+  document.getElementById("btn9").value == "X"
 ){
   alert("The Winner is X");
   reset();
