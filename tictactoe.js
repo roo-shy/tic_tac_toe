5eda24d4ccaa08a65d1310366a791c6cc81b09c0
at-rooshee <ru_sha_nia@yahoo.ca>
2016-12-31 00:06:05 -0600

    Editing js to a lighter version

--- tictactoe.js
+++ tictactoe.js
@@ -1,52 +1,32 @@
-var player = 1;
-function clickBtn(btn) {
-  if (player == 1) {
-  document.getElementById(btn).value = "X";
-  document.getElementById(btn).disabled = "disabled";
-  player -= 1;
-  winner();
-  } 
-  else {
-  document.getElementById(btn).value = "0";
-  document.getElementById(btn).disabled = "disabled";
-  player += 1;
-  winner();
-  }
+var canvas, ctx;
+
+window.onload = function main() {
+  canvas = document.createElement("canvas");
+  canvas.width = canvas.height = 200;
+  ctx = canvas.getContext("2d");
+  
+  document.body.appendChild(canvas);
+  
+  init();
+  tick();
+  
 }
 
-function winner() {
-  if(document.getElementById("btn1").value == "X" && 
-  document.getElementById("btn2").value == "X" &&
-  document.getElementById("btn3").value == "X" ||
-  document.getElementById("btn4").value == "X" && 
-  document.getElementById("btn5").value == "X" &&
-  document.getElementById("btn6").value == "X" ||
-  document.getElementById("btn7").value == "X" && 
-  document.getElementById("btn8").value == "X" &&
-  document.getElementById("btn9").value == "X" ||
-  document.getElementById("btn1").value == "X" && 
-  document.getElementById("btn4").value == "X" &&
-  document.getElementById("btn7").value == "X" ||
-  document.getElementById("btn2").value == "X" && 
-  document.getElementById("btn5").value == "X" &&
-  document.getElementById("btn8").value == "X" ||
-  document.getElementById("btn3").value == "X" && 
-  document.getElementById("btn6").value == "X" &&
-  document.getElementById("btn9").value == "X"
-){
-  alert("The Winner is X");
-  reset();
-}
+function init() {
+  
 }
 
-function reset() {
-  document.getElementById("btn1").value = "";
-  document.getElementById("btn2").value = "";
-  document.getElementById("btn3").value = "";
-  document.getElementById("btn4").value = "";
-  document.getElementById("btn5").value = "";
-  document.getElementById("btn6").value = "";
-  document.getElementById("btn7").value = "";
-  document.getElementById("btn8").value = "";
-  document.getElementById("btn9").value = "";
+function tick() {
+  window.requestAnimationFrame(tick);
+  
+  update();
+  render();
+}
+
+function update() {
+  
+}
+
+function render() {
+  
 }
