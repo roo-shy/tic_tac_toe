d14973094364a0c486bd0b83552e883bd4d97ffa
at-rooshee <ru_sha_nia@yahoo.ca>
2017-01-01 22:58:20 -0600

    Starting js from scratch no canvas

--- tictactoe.js
+++ tictactoe.js
@@ -1,16 +1,16 @@
-var canvas, ctx;
-
-window.onload = function main() {
-  canvas = document.createElement("canvas");
-  canvas.width = canvas.height = 200;
-  ctx = canvas.getContext("2d");
-  
-  document.body.appendChild(canvas);
-  
-  init();
-  tick();
-  
-}
+// var canvas, ctx;
+// 
+// window.onload = function main() {
+//   canvas = document.createElement("canvas");
+//   canvas.width = canvas.height = 200;
+//   ctx = canvas.getContext("2d");
+//   
+//   document.body.appendChild(canvas);
+//   
+//   init();
+//   tick();
+//   
+// }
 
 function init() {
   
